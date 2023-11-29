69fd16508d7eb291c275a7bbaa11e9e9148bcc9e
Rahul Verma <rahulverma@Rahuls-MacBook-Pro.local>
2023-11-29 11:00:14 +0530

    Completed JS-1

---
+++ 04_JAVASCRIPT/JS_1/06_practiceQs.js
@@ -0,0 +1,16 @@
+//Question 1
+//What is the value of age after this code runs
+let age = 23;
+age + 2; //after 2 yrs
+console.log(age);
+
+//Problem 2
+// Value of Avg after this code runs 
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = (hindi + eng + math) / 3;
+console.log(avg);
+
+let sun = 1 + 2 ; //this is the code for the sum 
+
